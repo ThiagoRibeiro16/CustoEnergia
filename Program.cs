4879d6964d916b787272dc526b8515013be59e6b
ThiagoRibeiro16 <thiago.escola321@gmail.com>
2025-03-30 22:51:53 -0300

    finaliza o programa

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+double consumo, horasUso, custo, custoEstimado;
+
+Console.Write("Consumo do aparelho: ");
+consumo = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Horas de uso por dia: ");
+horasUso = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Custo da energia: ");
+custo = Convert.ToDouble(Console.ReadLine());
+
+custoEstimado = (consumo * horasUso) * custo;
+
+Console.WriteLine($"Custo estimado: {custoEstimado:N2}");
