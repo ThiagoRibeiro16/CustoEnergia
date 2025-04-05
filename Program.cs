e36e30dc509b7995080becd3458a021328e320f0
ThiagoRibeiro16 <thiago.escola321@gmail.com>
2025-04-05 17:57:22 -0300

    Add correções ao programa

--- Program.cs
+++ Program.cs
@@ -3,13 +3,13 @@
 double consumo, horasUso, custo, custoEstimado;
 
 Console.Write("Consumo do aparelho: ");
-consumo = Convert.ToDouble(Console.ReadLine());
+consumo = Convert.ToDouble(Console.ReadLine()!);
 
 Console.Write("Horas de uso por dia: ");
-horasUso = Convert.ToDouble(Console.ReadLine());
+horasUso = Convert.ToDouble(Console.ReadLine()!);
 
 Console.Write("Custo da energia: ");
-custo = Convert.ToDouble(Console.ReadLine());
+custo = Convert.ToDouble(Console.ReadLine()!);
 
 custoEstimado = (consumo * horasUso) * custo;
 
